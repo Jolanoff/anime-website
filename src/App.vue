ec93379166146c3137398c996ab6b944a9674c8f
Jolanoff <jawlan.abozaid12@gmail.com>
2022-08-01 19:39:10 +0200

    too much shit

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,24 @@ import PageFooter from './components/Items/PageFooter.vue'
 export default {
   name: 'App',
   components: {
-    NavBar,PageFooter
+    NavBar, PageFooter
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@300&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Josefin Sans', sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+}
+
+body {
+  background-color: white;
+}
 </style>
